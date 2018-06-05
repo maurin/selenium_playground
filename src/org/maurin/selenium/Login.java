f951b80fd12ea2c2145cad92c1d07698b06f5037
pablo a maurin <pablo@maurin.org>
2018-06-05 07:12:18 -0600

    Base Selenium source

--- src/org/maurin/selenium/Login.java
+++ src/org/maurin/selenium/Login.java
@@ -1,10 +1,22 @@
 package org.maurin.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Login {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		// Load Chromedriver program
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		System.setProperty("webdriver.chrome.driver", "tools/chromedriver");
 
+		// Open WebDriver
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to web page
+		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+
+		//driver.close();
 	}
 
 }

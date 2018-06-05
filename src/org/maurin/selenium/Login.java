ea60051f6979e66a59352a78c9992fcaebfd7c14
pablo a maurin <pablo@maurin.org>
2018-06-05 07:39:29 -0600

    Enter form data

--- src/org/maurin/selenium/Login.java
+++ src/org/maurin/selenium/Login.java
@@ -1,20 +1,34 @@
 package org.maurin.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 
 	public static void main(String[] args) {
-		// Load Chromedriver program
+		// Load Chromedriver program and open WebDriver
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 		System.setProperty("webdriver.chrome.driver", "tools/chromedriver");
 
-		// Open WebDriver
 		WebDriver driver = new ChromeDriver();
 
-		// Navigate to web page
+		// Open browser and Navigate to web page
 		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+
+		// Find element, determine action, pass parameters
+		driver.findElement(By.name("ctl00$MainContent$txtUserName")).sendKeys("tim@testmail.com");
+		driver.findElement(By.name("ctl00$MainContent$txtPassword")).sendKeys("trpass");
+
+		driver.findElement(By.name("ctl00$MainContent$btnLogin")).click();
+
+
+
+
+
+
+
+
 
 		//driver.close();
 	}
